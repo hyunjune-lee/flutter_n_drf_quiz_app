8a1e8e70534af55ecc39d08533d182d9feb7c495
hylee <hylee@student.42seoul.kr>
2020-08-14 15:37:48 +0900

    quiz screen 완성

---
+++ lib/widget/widget_candidate.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CandWidget extends StatefulWidget {
+  //VoidCallback 은 CandWidget을 사용하는 부모 위젯에서
+  // 지정한 onTap을 전달해주는 기능을 함
+  VoidCallback tap;
+  String text;
+  int index;
+  double width;
+  bool answerState;
+
+  CandWidget({this.tap, this.index, this.width, this.text, this.answerState});
+  _CandWidgetState createState() => _CandWidgetState();
+}
+
+class _CandWidgetState extends State<CandWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width * 0.8,
+      height: widget.width * 0.1,
+      padding: EdgeInsets.fromLTRB(
+        widget.width * 0.048,
+        widget.width * 0.024,
+        widget.width * 0.048,
+        widget.width * 0.024,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.deepPurple),
+        //선택지 선택시 보라색, 선택되지 않았으면 하얀색으로 표시
+        color: widget.answerState ? Colors.deepPurple : Colors.white,
+      ),
+      child: InkWell(
+        child: Text(
+          widget.text,
+          style: TextStyle(
+            fontSize: widget.width * 0.035,
+            //택스트 마찬가지
+            color: widget.answerState ? Colors.white : Colors.black,
+          ),
+        ),
+        onTap: () {
+          setState(() {
+            widget.tap();
+            widget.answerState = !widget.answerState;
+          });
+        },
+      ),
+    );
+  }
+}
